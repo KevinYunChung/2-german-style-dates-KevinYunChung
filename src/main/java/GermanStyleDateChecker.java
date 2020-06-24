9828d69b8101c53b3cd1309f00513d28c025491d
Kevin Chung <43365809+KevinYunChung@users.noreply.github.com>
2020-06-24 22:03:39 +0200

    Finished
    Completed with difficulties since thinking about Schaltjahr and that some months have less then 30-31 days is kinda complicated

--- src/main/java/GermanStyleDateChecker.java
+++ src/main/java/GermanStyleDateChecker.java
@@ -3,6 +3,11 @@ import java.util.regex.Pattern;
 
 public class GermanStyleDateChecker {
     public static Matcher getMatcher(String dateString) {
-        return null;
+        String regex = "^([1-9]|0[1-9]|(1|2)[0-9]|3[01])[\\s-.]([1-9]|0[1-9]|1[012])[\\s-.](19|20)?\\d{2}$";
+        Pattern pattern = Pattern.compile(regex);
+
+        Matcher m = pattern.matcher(dateString);
+
+        return m;
     }
 }
